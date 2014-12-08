9a8e588941972e8e453e8ddb82283f848247c1f3
Dayne Davis <daynedavis5@gmail.com>
2014-12-08 13:20:45 -0500

    updated server.js to use passport for authentification

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var express  = require('express');
 var app      = express(); 								// create our app w/ express
 var mongoose = require('mongoose'); 					// mongoose for mongodb
 var port  	 = process.env.PORT || 8080; 				// set the port
+var passport = require('passport');
+var flash = require('connect-flash');
 var database = require('./config/database'); 			// load the database config
 var morgan   = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var session = require('express-session');
 
 // configuration ===============================================================
 mongoose.connect(database.url);
@@ -17,6 +20,12 @@ app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+app.use(cookieParser());
+
+app.use(session({ secret: 'ilovescotchscothcyscotchdayne'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 // routes ======================================================================
 require('./app/routes.js')(app);
