5c8f0386277a5fcc978929ccb09ef85adfd3e4b0
Dayne Davis <daynedavis5@gmail.com>
2014-12-08 19:40:34 -0500

    added cookie parser var

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var morgan   = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var session = require('express-session');
+var cookieParser = require('cookie-parser');
 
 // configuration ===============================================================
 mongoose.connect(database.url);
