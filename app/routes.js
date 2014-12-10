72cda2dfb84583732d42dfe298b503fde7f1bcb9
Dayne Davis <daynedavis5@gmail.com>
2014-12-09 21:33:07 -0500

    beautified files

--- app/routes.js
+++ app/routes.js
@@ -3,172 +3,186 @@ var Tag = require('./models/tag');
 
 module.exports = function(app, passport) {
 
-// api ===================================================================
+  // api ===================================================================
 
-// Users -----------------------------------------------------------------
+  // Users -----------------------------------------------------------------
 
-// get all todos
-app.get('/api/user', function(req, res) {
+  // get all todos
+  app.get('/api/user', function(req, res) {
 
-  // use mongoose to get all todos in the database
-  User.find(function(err, user) {
-
-    // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-    if (err)
-      res.send(err);
-
-    res.json(user); // return all user in JSON format
-  });
-});
-
-// create todo and send back all user after creation
-app.post('/api/user', function(req, res) {
-
-  // create a todo, information comes from AJAX request from Angular
-  User.create({
-    username: req.body.username,
-    password: req.body.password,
-    peerID: req.body.peerID,
-    done: false
-  }, function(err, todo) {
-    if (err)
-      res.send(err);
-
-    // get and return all the user after you create another
+    // use mongoose to get all todos in the database
     User.find(function(err, user) {
+
+      // if there is an error retrieving, send the error. nothing after res.send(err) will execute
       if (err)
         res.send(err);
-      res.json(user);
+
+      res.json(user); // return all user in JSON format
     });
   });
 
-});
+  // create todo and send back all user after creation
+  app.post('/api/user', function(req, res) {
 
-// delete a todo
-app.delete('/api/user/:user_id', function(req, res) {
-  User.remove({
-    _id: req.params.todo_id
-  }, function(err, todo) {
-    if (err)
-      res.send(err);
-
-    // get and return all the user after you create another
-    User.find(function(err, users) {
+    // create a todo, information comes from AJAX request from Angular
+    User.create({
+      username: req.body.username,
+      password: req.body.password,
+      peerID: req.body.peerID,
+      done: false
+    }, function(err, todo) {
       if (err)
         res.send(err);
-      res.json(users);
+
+      // get and return all the user after you create another
+      User.find(function(err, user) {
+        if (err)
+          res.send(err);
+        res.json(user);
+      });
+    });
+
+  });
+
+  // delete a todo
+  app.delete('/api/user/:user_id', function(req, res) {
+    User.remove({
+      _id: req.params.todo_id
+    }, function(err, todo) {
+      if (err)
+        res.send(err);
+
+      // get and return all the user after you create another
+      User.find(function(err, users) {
+        if (err)
+          res.send(err);
+        res.json(users);
+      });
     });
   });
-});
 
-// update peerID
-app.put('/api/user/:username/:peer_id', function(req, res) {
-  var query = User.where({username: req.params.username});
-  query.findOne(function(err, user) {
+  // update peerID
+  app.put('/api/user/:username/:peer_id', function(req, res) {
+    var query = User.where({
+      username: req.params.username
+    });
+    query.findOne(function(err, user) {
       // if there are any errors, return the error
       if (err)
         res.send(err);
 
-        user.peerID = req.params.peer_id;
-        user.save(function(err) {
-          if (err)
-            res.send(err);
+      user.peerID = req.params.peer_id;
+      user.save(function(err) {
+        if (err)
+          res.send(err);
 
-            res.json({ message: 'User updated!' });
-          });
-          // check to see if theres already a user with that email
-
+        res.json({
+          message: 'User updated!'
         });
-});
-
-// get peerID from username
-app.get('/api/user/:username', function(req, res) {
-  var query = User.where({username: req.params.username});
-  query.findOne(function(err, user) {
-    // if there are any errors, return the error
-    if (err)
-      res.send(err);
-    res.send(user.peerID);
-
       });
-    });
+      // check to see if theres already a user with that email
 
-// get username from peerID
-app.get('/api/ID/:peerID', function(req, res) {
-  var query = User.where({peerID: req.params.peerID});
-  query.findOne(function(err, user) {
-    // if there are any errors, return the error
-    if (err)
-      res.send(err);
+    });
+  });
+
+  // get peerID from username
+  app.get('/api/user/:username', function(req, res) {
+    var query = User.where({
+      username: req.params.username
+    });
+    query.findOne(function(err, user) {
+      // if there are any errors, return the error
+      if (err)
+        res.send(err);
+      res.send(user.peerID);
+
+    });
+  });
+
+  // get username from peerID
+  app.get('/api/ID/:peerID', function(req, res) {
+    var query = User.where({
+      peerID: req.params.peerID
+    });
+    query.findOne(function(err, user) {
+      // if there are any errors, return the error
+      if (err)
+        res.send(err);
       res.send(user.username);
 
     });
   });
 
-// Tags -------------------------------------------------------------------
+  // Tags -------------------------------------------------------------------
 
-// get all tags
-app.get('/api/tag', function(req, res) {
+  // get all tags
+  app.get('/api/tag', function(req, res) {
 
-  // use mongoose to get all todos in the database
-  Tag.find(function(err, tag) {
+    // use mongoose to get all todos in the database
+    Tag.find(function(err, tag) {
 
-    // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-    if (err)
-      res.send(err);
+      // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+      if (err)
+        res.send(err);
 
       res.json(tag); // return all user in JSON format
     });
   });
 
-// Create tag
-app.post('/api/tag/:tag', function(req, res) {
+  // Create tag
+  app.post('/api/tag/:tag', function(req, res) {
 
-  // create a todo, information comes from AJAX request from Angular
-  Tag.create({
-    tag: req.params.tag,
-    done: false
-  }, function(err, tag) {
-    if (err)
-      res.send(err);
+    // create a todo, information comes from AJAX request from Angular
+    Tag.create({
+      tag: req.params.tag,
+      done: false
+    }, function(err, tag) {
+      if (err)
+        res.send(err);
 
       // get and return all the user after you create another
       Tag.find(function(err, tag) {
         if (err)
           res.send(err);
-          res.json(tag);
-        });
+        res.json(tag);
       });
-
     });
 
-// add peerID to tag
-app.put('/api/tag/:tag/:peer_id', function(req, res) {
-  var query = Tag.where({tag: req.params.tag});
-  query.findOne(function(err, tag) {
-    // if there are any errors, return the error
-    if (err)
-      res.send(err);
+  });
+
+  // add peerID to tag
+  app.put('/api/tag/:tag/:peer_id', function(req, res) {
+    var query = Tag.where({
+      tag: req.params.tag
+    });
+    query.findOne(function(err, tag) {
+      // if there are any errors, return the error
+      if (err)
+        res.send(err);
 
       tag.peerIDs.push(req.params.peer_id);
       tag.save(function(err) {
         if (err)
           res.send(err);
 
-          res.json({ message: 'peerID added!' });
+        res.json({
+          message: 'peerID added!'
         });
-        // check to see if theres already a user with that email
-
       });
-    });
+      // check to see if theres already a user with that email
 
-// remove peerID from tag
-app.delete('/api/tag/:tag/:peer_id', function(req, res) {
-  var query = Tag.where({tag: req.params.tag});
-  query.findOne(function(err, tag) {
-    // if there are any errors, return the error
-    if (err)
-      res.send(err);
+    });
+  });
+
+  // remove peerID from tag
+  app.delete('/api/tag/:tag/:peer_id', function(req, res) {
+    var query = Tag.where({
+      tag: req.params.tag
+    });
+    query.findOne(function(err, tag) {
+      // if there are any errors, return the error
+      if (err)
+        res.send(err);
 
       var index = tag.peerIDs.indexOf(req.params.peer_id);
       if (index > -1) {
@@ -177,72 +191,75 @@ app.delete('/api/tag/:tag/:peer_id', function(req, res) {
           if (err)
             res.send(err);
 
-            res.json({ message: 'peerID removed!' });
+          res.json({
+            message: 'peerID removed!'
           });
-      }
-      else {
-        res.json({message: 'peerID does not exist'});
+        });
+      } else {
+        res.json({
+          message: 'peerID does not exist'
+        });
       }
 
-      });
     });
+  });
 
-// delete a tag
-app.delete('/api/tag/:tag_id', function(req, res) {
-  Tag.remove({
-    _id: req.params.tag_id
-  }, function(err, tag) {
-    if (err)
-      res.send(err);
+  // delete a tag
+  app.delete('/api/tag/:tag_id', function(req, res) {
+    Tag.remove({
+      _id: req.params.tag_id
+    }, function(err, tag) {
+      if (err)
+        res.send(err);
 
       // get and return all the user after you create another
       Tag.find(function(err, tags) {
         if (err)
           res.send(err);
-          res.json(tags);
-        });
+        res.json(tags);
       });
     });
+  });
 
-// End API =================================================================
+  // End API =================================================================
 
 
-// application -------------------------------------------------------------
+  // application -------------------------------------------------------------
 
-app.get('/login', function(req, res) {
-  res.sendfile('./public/login.html'); // load the single view file (angular will handle the page changes on the front-end)
-});
+  app.get('/login', function(req, res) {
+    res.sendfile('./public/login.html'); // load the single view file (angular will handle the page changes on the front-end)
+  });
 
-app.get('/register', function(req, res) {
-  res.sendfile('./public/register.html'); // load the single view file (angular will handle the page changes on the front-end)
-});
+  app.get('/register', function(req, res) {
+    res.sendfile('./public/register.html'); // load the single view file (angular will handle the page changes on the front-end)
+  });
 
-app.get('/', isLoggedIn, function(req, res) {
-  res.sendfile('./public/app.html', {
-    user: req.user
-  }); // load the single view file (angular will handle the page changes on the front-end)
-});
+  app.get('/', isLoggedIn, function(req, res) {
+    res.sendfile('./public/app.html', {
+      user: req.user
+    }); // load the single view file (angular will handle the page changes on the front-end)
+  });
 
-app.get('/logout', function(req, res) {
-  req.logOut();
-  res.redirect('/login');
-});
+  app.get('/logout', function(req, res) {
+    req.logOut();
+    res.redirect('/login');
+  });
 
-app.post('/register', passport.authenticate('local-signup', {
-  successRedirect : '/',
+  app.post('/register', passport.authenticate('local-signup', {
+    successRedirect: '/',
 
-  failureRedirect : '/register',
+    failureRedirect: '/register',
 
-  failureFlash : true
-}));
+    failureFlash: true
+  }));
 
-app.post('/login', passport.authenticate('local-login', {
-  successRedirect : '/',
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/',
 
-  failureRedirect : '/login',
+    failureRedirect: '/login',
 
-  failureFlash : true
-}));
+    failureFlash: true
+  }));
 
 };
 
